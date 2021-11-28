84f003d954c033e926ba846b545e6a389be83220
Dylan-Hayes-GitHub <dylanmhayes12@gmail.com>
2021-11-28 17:36:16 +0000

    adding layout style sheets and html

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- Designed by dylan -->
+<footer>
+
+<div class="footerContent">
+    <h3>Contact Us!</h3>
+    <p>Please dont ever be hesistant in contacting us!</p>
+    <p>Phone No: 061-123456</p>
+    
+</div>
+
+<div class="footerContent">
+<h3>Want to become one of our partners?</h3>
+    <p>Apply Bellow!</p>
+    <p><button class="footerButton">Apply to become a vendor</button></p>
+</div>
+
+
+</footer>
+</body>
+</html>
+<!-- Designed by dylan -->
---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<!-- Designed by dylan -->
+<head>
+    <meta charset="utf-8">
+
+
+    <script src="https://kit.fontawesome.com/70b0930f93.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+
+        <div class="headerWrapper">
+            <div class="companyLogo">
+                <img src="<?php echo $images . 'rvemLogo.jfif'; ?>">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>HOME</li>
+                    <li>WHY RVEM<i class="fas fa-angle-down"></i></li>
+                    <li>CREATE EVENT</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </div>
+    </header><!-- Designed by dylan -->
